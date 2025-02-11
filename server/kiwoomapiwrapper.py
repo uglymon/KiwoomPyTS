a7a95956bb22b0217cf76a0e91d7c8eb5e4e13f1
uglymon <uglymon@gmail.com>
2025-02-11 15:33:08 +0900

    Add GetChejanData method to KiwoomAPIWrapper

--- server/kiwoomapiwrapper.py
+++ server/kiwoomapiwrapper.py
@@ -440,6 +440,18 @@ class KiwoomAPIWrapper:
             sOrgOrderNo,
         )
 
+    def GetChejanData(self, nFid: int) -> str:
+        """
+        BSTR GetChejanData(
+            long nFid   // 실시간 타입에 포함된 FID(Field ID)
+        )
+
+        OnReceiveChejan()이벤트가 발생될때 FID에 해당되는 값을 구하는 함수입니다.
+        이 함수는 OnReceiveChejan() 이벤트 안에서 사용해야 합니다.
+        예) 체결가 = GetChejanData(910)
+        """
+        return self.ocx.dynamicCall("GetChejanData(int)", nFid)
+
     def GetConditionLoad(self) -> int:
         """
         long GetConditionLoad()
